b5f4109b9d311cc32960bfc913e99ca4a0a8365b
Arsen Losenko <arsenlosenko@gmail.com>
2018-01-25 22:10:29 +0200

    Described logic of app as a class

--- js/main.js
+++ js/main.js
@@ -1,16 +1,56 @@
+class SavedArticle {
+    constructor(base) {
+        this.url = "";
+        this.time = "";
+        this.base = base;
+        this.title = "";
+        this.icon = ""
+    }
 
-function dateChange(e) {
-    let checkedDate = e.target.value;
-    let parsedDate = new Date(checkedDate);
-    console.log(Object.getOwnPropertyNames(parsedDate).sort());
-}
+    createTextInput() {
+        let urlInput = document.createElement('input');
+        urlInput.type = 'text';
+        urlInput.placeholder = 'Enter URL here';
+        return urlInput;
+    }
+
+    createDateInput() {
+        let dateInput = document.createElement('input');
+        dateInput.type = 'datetime-local';
+        return dateInput;
+    }
+
+    createSubmitBtn() {
+        let btn = document.createElement('button');
+        btn.textContent = 'Add';
+        btn.addEventListener('click', ()=>{
+           this.render();
+        });
+        return btn;
+    }
 
 
-function main() {
-    console.log('task started')
+    createForm() {
+        let form = document.createElement('form');
+        let input = this.createTextInput();
+        let date = this.createDateInput();
+        let btn = this.createSubmitBtn();
+        form.appendChild(input);
+        form.appendChild(date);
+        form.appendChild(btn);
+        return form;
+    }
+
+    render() {
+        let form = this.createForm();
+        this.base.appendChild(form);
+        console.log(this.base, form);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('.dateInput').onchange = dateChange;
-  main();
+    let base = document.getElementById('app');
+    let article = new SavedArticle(base);
+    article.render();
 });
+
