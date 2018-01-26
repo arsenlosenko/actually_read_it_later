3ea98b639a971f1c33b9237bfb92951aea622868
Arsen Losenko <arsenlosenko@gmail.com>
2018-01-26 10:42:44 +0200

    Refactored class, added addmore button

--- js/main.js
+++ js/main.js
@@ -1,56 +1,58 @@
 class SavedArticle {
     constructor(base) {
-        this.url = "";
-        this.time = "";
         this.base = base;
-        this.title = "";
-        this.icon = ""
+        this.render()
     }
 
-    createTextInput() {
+    render() {
+        this.createAddMoreBtn();
+        this.base.appendChild(SavedArticle.createFormEntry());
+        console.log(this.base);
+    }
+
+    createAddMoreBtn() {
+        let addMoreBtn = document.createElement('button');
+        let div = document.getElementById('addMoreBtn');
+        addMoreBtn.textContent = '+';
+        addMoreBtn.addEventListener('click', () => {
+            this.base.appendChild(SavedArticle.createFormEntry());
+        });
+
+        div.appendChild(addMoreBtn);
+        return div
+    }
+
+    static createFormEntry() {
+        let div = document.createElement('div');
+        div.appendChild(SavedArticle.createTextInput());
+        div.appendChild(SavedArticle.createDateInput());
+        div.appendChild(SavedArticle.createSubmitBtn());
+        return div;
+    }
+
+    static createTextInput() {
         let urlInput = document.createElement('input');
         urlInput.type = 'text';
         urlInput.placeholder = 'Enter URL here';
         return urlInput;
     }
 
-    createDateInput() {
+    static createDateInput() {
         let dateInput = document.createElement('input');
         dateInput.type = 'datetime-local';
         return dateInput;
     }
 
-    createSubmitBtn() {
+    static createSubmitBtn() {
         let btn = document.createElement('button');
         btn.textContent = 'Add';
-        btn.addEventListener('click', ()=>{
-           this.render();
-        });
         return btn;
     }
 
-
-    createForm() {
-        let form = document.createElement('form');
-        let input = this.createTextInput();
-        let date = this.createDateInput();
-        let btn = this.createSubmitBtn();
-        form.appendChild(input);
-        form.appendChild(date);
-        form.appendChild(btn);
-        return form;
-    }
-
-    render() {
-        let form = this.createForm();
-        this.base.appendChild(form);
-        console.log(this.base, form);
-    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     let base = document.getElementById('app');
-    let article = new SavedArticle(base);
-    article.render();
+    new SavedArticle(base);
 });
 
