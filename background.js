ad67ee8561dbe7c06b9b38f469087bc7fdf8af98
Arsen Losenko <arsenlosenko@gmail.com>
2018-02-06 22:43:48 +0200

    Passed needed time and url to storage sync

--- background.js
+++ background.js
@@ -2,21 +2,21 @@
 
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
-    let url = '';
+    // TODO: pass required url to notification, on click - open tab with url
     chrome.storage.sync.get('url', function(item){
-       url = item.url;
-   chrome.notifications.create({
-       type:     'basic',
-       iconUrl:  'stay_hydrated.png',
-       title:    'Time to Hydrate',
-       message:   url, 
-     buttons: [
-       {title: 'Keep it Flowing.'},
-       {title: 'Test button 2'}
-       ],
-       priority: 0});
+        console.log(item);
+        chrome.notifications.create({
+            type:     'basic',
+            iconUrl:  'stay_hydrated.png',
+            title:    'Time to Hydrate',
+            message:   item.url , 
+            buttons: [
+            {title: 'Keep it Flowing.'},
+           {title: 'Test button 2'}
+           ],
+           priority: 0});
+        });
     });
-});
 
 chrome.notifications.onButtonClicked.addListener(function() {
   chrome.storage.sync.get(function(items) {
--- popup.js
+++ popup.js
@@ -22,13 +22,26 @@ function clearAlarm() {
   window.close();
 }
 
+
+function getTimeDiff(){
+    let time  = document.getElementById('dateTime');
+    let dateNow = new Date();
+    let enteredDate = new Date(time.value);
+    let diffMinutes = Math.ceil((enteredDate.getTime() - dateNow.getTime()) / 60000);
+    return diffMinutes;
+}
+
 function setUrl(){
+    // TODO: refactor to look better, and save url accordignly to alarm 
     let url  = document.getElementById('urlInput');
+    let minutes = getTimeDiff();
+    console.log(minutes);
     chrome.alarms.create("testAlarm", {delayInMinutes: 0.1});
     console.log("Alarm is fired");
     chrome.storage.sync.set({'url': url.value}, function(){
         alert(url.value + 'is in storage'); 
     });
+    chrome.alarms.create({delayInMinutes: minutes});
 }
 
 
