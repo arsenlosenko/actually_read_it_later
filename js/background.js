58aa0c987a8a8bcc5527f02cb66c9074dc6cf495
Arsen Losenko <arsenlosenko@gmail.com>
2018-02-08 23:30:34 +0200

    Connected inputs with setAlarm button

--- js/background.js
+++ js/background.js
@@ -1,23 +1,22 @@
 'use strict';
 // TODO: save current state of app from click to click
 // TODO: set default time in datetime-local to today
-// TODO: refactor JS
 // TODO: add analytics
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
-    chrome.storage.sync.get('url', function(item){
-        console.log(item);
-        chrome.notifications.create({
-            type:     'basic',
-            iconUrl:  '../img/notebook256.png',
-            title:    'Time to Read',
-            message:   item.url , 
-            buttons: [
-            {title: 'Read Article'},
-           {title: 'Test button 2'}
-           ],
-           priority: 0});
-        });
+    let urlKey = alarm.name + 'url';
+
+    chrome.storage.sync.get(urlKey,function(item){
+         chrome.notifications.create({
+             type:     'basic',
+             iconUrl:  '../img/notebook256.png',
+             title:    'Time to Read',
+             message:   item[urlKey] , 
+             buttons: [
+                 {title: 'Read Article'}
+            ],
+            priority: 0});
+    });
     });
 
 chrome.notifications.onButtonClicked.addListener(function(buttonIndex) {
@@ -29,4 +28,3 @@ chrome.notifications.onButtonClicked.addListener(function(buttonIndex) {
     });
   });
 });
-
--- js/popup.js
+++ js/popup.js
@@ -1,51 +1,33 @@
 'use strict';
 
-function setAlarm(event) {
-  let minutes = parseFloat(event.target.value);
-  chrome.browserAction.setBadgeText({text: 'ON'});
-  chrome.alarms.create({delayInMinutes: minutes});
-  chrome.storage.sync.set({minutes: minutes});
 
-  let url  = document.getElementById('urlInput');
-  chrome.storage.sync.set({url: url.value}, function(){
-    console.log(chrome.storage.sync.get(function(items){
-     console.log(items)
-  }));
-  }); 
-  
-  window.close();
-}
-
-function clearAlarm() {
-  chrome.browserAction.setBadgeText({text: ''});
-  chrome.alarms.clearAll();
-  window.close();
-}
-
-
-function getTimeDiff(){
-    let time  = document.getElementById('dateTime');
+function getTimeDiff(time){
     let dateNow = new Date();
-    let enteredDate = new Date(time.value);
+    let enteredDate = new Date(time);
     let diffMinutes = Math.ceil((enteredDate.getTime() - dateNow.getTime()) / 60000);
     return diffMinutes;
 }
 
-function setUrl(){
-    // TODO: refactor to look better, and save url accordignly to alarm 
-    let url  = document.getElementById('urlInput');
-    let minutes = getTimeDiff();
-    console.log(minutes);
-    chrome.alarms.create("testAlarm", {delayInMinutes: 0.1});
-    console.log("Alarm is fired");
-    chrome.storage.sync.set({'url': url.value}, function(){
-        alert(url.value + 'is in storage'); 
+function setAlarm(url, minutes, key){
+    let alarmName = 'alarm'+key;
+    let urlKey = alarmName + 'url'; 
+    let obj = {};
+    obj[urlKey] = url;
+
+    chrome.storage.sync.set(obj , function(){
+        console.log(obj);
     });
-    chrome.alarms.create({delayInMinutes: minutes});
+    chrome.alarms.create(alarmName, {delayInMinutes: minutes});
 }
 
 
-// document.getElementById('sampleSecond').addEventListener('click', setAlarm);
-// document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
-document.getElementById('urlBtn').addEventListener('click', setUrl);
-
+document.querySelectorAll('.urlBtn').forEach(function(item){
+    item.addEventListener('click', function(){
+        let key = this.dataset.key;
+        let readingTime = document.querySelector('.dateTime'+ key).value;
+        let articleUrl = document.querySelector('.urlInput' + key).value;
+        let minutes = getTimeDiff(readingTime);
+    
+        setAlarm(articleUrl, minutes, key);
+    });
+});
